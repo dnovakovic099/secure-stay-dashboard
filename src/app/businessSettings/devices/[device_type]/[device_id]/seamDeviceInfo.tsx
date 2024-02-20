cb685fc0d3b3f8434f265e4ef4d7200ef1d59886
Prasanna <prasannakb440@gmail.com>
2024-02-20 21:41:08 +0545

    feature/integrated-sifely-locks

---
+++ src/app/businessSettings/devices/[device_type]/[device_id]/seamDeviceInfo.tsx
@@ -0,0 +1,30 @@
+import { envConfig } from '@/utility/environment';
+import { SeamProvider, DeviceDetails } from '@seamapi/react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const SeamDeviceInfo = ({ device_id }: any) => {
+    const [clientSessionToken, setClientSessionToken] = useState("");
+
+    const getToken = async () => {
+        const apiUrl = `${envConfig.backendUrl}/device/getclientsessiontoken`
+        const res = await axios.get(apiUrl);
+        if (res.status == 200) {
+            setClientSessionToken(res.data.token);
+        }
+    };
+
+    useEffect(() => {
+        getToken()
+    }, [])
+
+    return (
+        <div>
+            <SeamProvider clientSessionToken={clientSessionToken}>
+                <DeviceDetails className="" disableResourceIds deviceId={device_id} />
+            </SeamProvider>
+        </div>
+    )
+}
+
+export default SeamDeviceInfo
