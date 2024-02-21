070bd4d77d0369b0d025f1b37932343521b9faa2
Prasanna <prasannakb440@gmail.com>
2024-02-21 17:06:44 +0545

    ui-improvement

--- src/app/businessSettings/devices/form.tsx
+++ src/app/businessSettings/devices/form.tsx
@@ -35,38 +35,52 @@ const Form = (props: any) => {
     }
     
     return (
-        <div className=''>
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-                Username
-            </label>
-            <input
-                className="mb-3 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="username"
-                type="text"
-                placeholder="Username"
-                value={username}
-                onChange={(e) => handleUsernameChange(e.target.value)}
-            />
-            <div className="mb-6">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-                    Password
-                </label>
-                <input
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id="password"
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => handlePasswordChange(e.target.value)}
-                />
-            </div>
-            <div className="flex items-center justify-end mb-4">
-                <button
-                    onClick={() => handleSubmit()}
-                    className="w-full rounded-full bg-blue-900 px-2 py-1 text-base font-medium  text-white shadow-sm hover:bg-blue-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-800"
-                >
-                    Sign In
-                </button>
+        <div className=' px-5 py-6 rounded-md'>
+            <div className='text-center font-semibold -mt-14 text-indigo-600 py-2 text-2xl mb-6'>Login to Sifely Account</div>
+            <div className='flex space-x-5 '>
+                <div>
+                    <img
+                        src='https://www.schlage.com/content/dam/sch-us/homepage-refresh/smart-lock.png'
+                        alt=''
+                        height={80}
+                        width={110}
+                    />
+                </div>
+                <hr />
+                <div>
+                    <label className="block text-indigo-900 text-sm font-bold mb-2" htmlFor="username">
+                        Username
+                    </label>
+                    <input
+                        className="mb-3 shadow appearance-none border rounded w-full py-2 px-3 text-gray-900 leading-tight focus:outline-none focus:shadow-outline"
+                        id="username"
+                        type="text"
+                        placeholder="Username"
+                        value={username}
+                        onChange={(e) => handleUsernameChange(e.target.value)}
+                    />
+                    <div className="mb-6">
+                        <label className="block text-indigo-900 text-sm font-semibold mb-2" htmlFor="password">
+                            Password
+                        </label>
+                        <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-900 leading-tight focus:outline-none focus:shadow-outline text-base"
+                            id="password"
+                            type="password"
+                            placeholder="********"
+                            value={password}
+                            onChange={(e) => handlePasswordChange(e.target.value)}
+                        />
+                    </div>
+                    <div className="flex items-center justify-start ">
+                        <button
+                            onClick={() => handleSubmit()}
+                            className=" rounded-md w-full bg-indigo-600 px-10 py-1 text-base font-medium  text-white shadow-sm hover:bg-blue-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-800"
+                        >
+                            Sign In
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     )
