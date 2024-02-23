bf7bd4de95547d19984b5ee66459b2b9de377f2c
Amit kharel <amit.infoxit@gmail.com>
2024-02-23 22:53:59 +0545

    sifely lock ui design modification

--- src/app/businessSettings/devices/[device_type]/[device_id]/deviceDetails.tsx
+++ src/app/businessSettings/devices/[device_type]/[device_id]/deviceDetails.tsx
@@ -3,119 +3,135 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { envConfig } from "@/utility/environment";
-import { ChevronDownIcon, ChevronUpIcon, PencilSquareIcon } from "@heroicons/react/20/solid";
+import {
+  ChevronDownIcon,
+  ChevronUpIcon,
+  PencilSquareIcon,
+} from "@heroicons/react/20/solid";
 import toast from "react-hot-toast";
 import SeamDeviceInfo from "./seamDeviceInfo";
 import SifelyDeviceInfo from "./sifelyDeviceInfo";
 
 interface listing {
-  listingId: number,
-  id: number,
-  name: string,
-  externalListingName: string,
-  address: string,
-  price: number,
-  guestsIncluded: number,
-  priceForExtraPerson: number
-  currencyCode: string
-  images: Array<image>
+  listingId: number;
+  id: number;
+  name: string;
+  externalListingName: string;
+  address: string;
+  price: number;
+  guestsIncluded: number;
+  priceForExtraPerson: number;
+  currencyCode: string;
+  images: Array<image>;
 }
 
 interface image {
-  id: number,
-  caption: string,
-  vrboCaption: string,
-  airbnbCaption: string,
-  url: string,
-  sortOrder: number
+  id: number;
+  caption: string;
+  vrboCaption: string;
+  airbnbCaption: string;
+  url: string;
+  sortOrder: number;
 }
 
 interface DeviceInfo {
-  lockName: string,
-  modelNum: string
+  lockName: string;
+  modelNum: string;
 }
 
-
-
 const DeviceInfo = ({ device_type, device_id }: any) => {
-  const [listings, setListings] = useState([])
+  const [listings, setListings] = useState([]);
   const [selectedListing, setSelectedListing] = useState<number[]>([]);
-  const [connectedListings, setConnectedListings] = useState<listing[]>([])
-  const [showConnectedListings, setShowConnectedListings] = useState(false)
-  const [selection, setSelection] = useState(false)
-  const [filteredListings, setFilteredListings] = useState<listing[]>([])
+  const [connectedListings, setConnectedListings] = useState<listing[]>([]);
+  const [showConnectedListings, setShowConnectedListings] = useState(false);
+  const [selection, setSelection] = useState(false);
+  const [filteredListings, setFilteredListings] = useState<listing[]>([]);
 
-  const router = useRouter()
+  const router = useRouter();
+
+  
+  useEffect(() => {
+    getListings();
+    getDeviceListings();
+  }, []);
 
   const getListings = async () => {
-    const apiUrl = `${envConfig.backendUrl}/listing/getlistings`
-    const res = await axios.get(apiUrl)
+    const apiUrl = `${envConfig.backendUrl}/listing/getlistings`;
+    const res = await axios.get(apiUrl);
     if (res.status == 200) {
-      const listings = res.data.listings
-      setListings(listings)
+      const listings = res.data.listings;
+      setListings(listings);
     }
-  }
+  };
 
   const saveDeviceLockInfo = async () => {
-    const apiUrl = `${envConfig.backendUrl}/device/savelocklistinginfo`
-    const res = await axios.post(apiUrl, { device_id, listing_id: selectedListing[0] })
+    const apiUrl = `${envConfig.backendUrl}/device/savelocklistinginfo`;
+
+    const res = await axios.post(apiUrl, {
+      device_id,
+      listing_id: selectedListing[0],
+    });
+
     if (res.status == 200) {
       if (res.data.success) {
         toast.success(res.data.message)
       } else {
         toast.error(res.data.message)
       }
-      setSelection(false)
-      getDeviceListings()
+      setSelection(false);
+      getDeviceListings();
     }
-  }
+
+  };
 
   const getDeviceListings = async () => {
-    const apiUrl = `${envConfig.backendUrl}/device/getlistings/${device_id}`
-    const result = await axios.get(apiUrl)
+
+    const apiUrl = `${envConfig.backendUrl}/device/getlistings/${device_id}`;
+    const result = await axios.get(apiUrl);
+
     if (result.status == 200) {
-      setConnectedListings(result.data)
-      setFilteredListings(result.data)
-      let arr: number[] = []
+      setConnectedListings(result.data);
+      setFilteredListings(result.data);
+      let arr: number[] = [];
       result.data.map((res: listing) => {
-        arr.push(res.listingId)
-      })
-      setSelectedListing(arr)
+        arr.push(res.listingId);
+      });
+      setSelectedListing(arr);
     }
-  }
+
+  };
 
   const handleChange = (value: any) => {
     setSelectedListing([value])
-  }
+  };
 
   const handleEditClick = async (e: any) => {
-    e.stopPropagation()
-    setSelection(true)
-    setFilteredListings(listings)
-  }
+    e.stopPropagation();
+    setSelection(true);
+    setFilteredListings(listings);
+  };
 
-  useEffect(() => {
-    getListings()
-    getDeviceListings()
-  }, []);
 
   return (
-    <div className="bg-gray-100 rounded-md min-h-[682px] p-4">
+    <div className="bg-white rounded-md h-[100%] p-4">
       {/* <Toaster /> */}
       <div className="flex justify-between px-2">
         <h2 className="text-xl font-bold text-indigo-600">Device Details</h2>
         <div className="flex gap-2">
           <button
-            onClick={() => router.back()}
+            onClick={() => {
+              localStorage.setItem("locksPressed", "false"); // Set the desired value
+              router.back();
+            }}
             type="button"
-            className="rounded-full bg-red-600 w-16 px-2 py-1 text-xs  text-white shadow-sm hover:bg-red-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-800"
+            className="rounded-md bg-red-600 w-16 px-2 py-1 text-xs  text-white shadow-sm hover:bg-red-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-800"
           >
             Cancel
           </button>
           <button
             type="submit"
             onClick={() => saveDeviceLockInfo()}
-            className={`rounded-full bg-indigo-600 w-16 px-2 py-1 text-xs  text-white shadow-sm hover:bg-indigo-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-800`}
+            className={`rounded-md bg-indigo-600 w-16 px-2 py-1 text-xs  text-white shadow-sm hover:bg-indigo-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-800`}
           >
             Save
           </button>
@@ -123,48 +139,64 @@ const DeviceInfo = ({ device_type, device_id }: any) => {
       </div>
 
       <div>
-        {
-          device_type == 'seam' && <SeamDeviceInfo device_id={device_id} />
-        }
-        {
-          device_type == 'sifely' && <SifelyDeviceInfo device_id={device_id} />
-        }
-        <div onClick={() => setShowConnectedListings((prev) => !prev)} className="px-6 w-[91%] ml-6 bg-indigo-100 mb-5 py-2 rounded-md cursor-pointer select-none flex justify-between">
-          <p className="text-gray-500 font-medium text-sm">Connected Listings {`(${connectedListings.length})`}</p>
-          <div className="flex justify-end gap-4">
-            {showConnectedListings ?
-              (
-                <>
-                  <button
-                    onClick={(e) => handleEditClick(e)}
-                    className={`rounded-full flex gap-1 bg-indigo-600 w-16 px-2 py-1 text-xs  text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-800`}
-                  >
-                    <PencilSquareIcon height={15} width={15} /><span>Edit</span>
-                  </button>
-                  <ChevronUpIcon height={20} width={20} />
-                </>
-              )
-              :
-              (<ChevronDownIcon height={20} width={20} />)}
+        {device_type == "seam" && <SeamDeviceInfo device_id={device_id} />}
+        {device_type == "sifely" && <SifelyDeviceInfo device_id={device_id} />}
+        <div
+          onClick={() => setShowConnectedListings((prev) => !prev)}
+          className="px-6 w-[91%] mx-auto bg-indigo-100 mb-5 py-3 rounded-md cursor-pointer select-none flex justify-between items-center transition duration-300 ease-in-out transform hover:scale-105"
+        >
+          <p className="text-gray-700 font-semibold text-sm">
+            Connected Listings {`(${connectedListings.length})`}
+          </p>
+          <div className="flex items-center gap-4">
+            {showConnectedListings ? (
+              <>
+                <button
+                  onClick={(e) => handleEditClick(e)}
+                  className={`rounded-full flex items-center gap-1 bg-indigo-600 px-3 py-1 text-sm text-white shadow-md focus:outline-none focus:ring focus:border-indigo-300`}
+                >
+                  <PencilSquareIcon height={16} width={16} />
+                  <span>Edit</span>
+                </button>
+                <ChevronUpIcon height={20} width={20} />
+              </>
+            ) : (
+              <ChevronDownIcon height={20} width={20} />
+            )}
           </div>
         </div>
-        {
-          showConnectedListings && filteredListings.map((listing: listing) => (
-            <div className="flex gap-4 px-4 py-2 w-[85%] ml-3 items-center cursor-pointer select-none">
-              {selection && <input type="checkbox" onChange={() => handleChange(listing.listingId)} checked={selectedListing?.includes(listing.listingId)} name="" id="" className="w-[17px] h-[17px] cursor-pointer" />}
-              <img
-                src={listing.images[0].url}
-                height={60}
-                width={60}
-                className="rounded-md"
-              />
-              <div>
-                <h5 className="font-medium text-sm">{listing?.name.substring(0, 40) + '...'}</h5>
-                <small className="text-slate-400 text-xs">{listing?.address.substring(0, 40) + '...'}</small>
+
+        <div>
+          {showConnectedListings &&
+            filteredListings?.map((listing: listing) => (
+              <div
+                key={listing.listingId}
+                className="flex items-center gap-4 p-3 w-[85%] mx-auto mt-3 cursor-pointer select-none border rounded-md hover:shadow-md bg-white shadow-lg transition-transform transform hover:scale-105"
+              >
+                <input
+                  type="checkbox"
+                  onChange={() => handleChange(listing.listingId)}
+                  checked={selectedListing?.includes(listing.listingId)}
+                  className="w-4 h-4 border rounded cursor-pointer focus:ring focus:border-indigo-300"
+                />
+                <img
+                  src={listing.images[0].url}
+                  alt={listing.name}
+                  height={40}
+                  width={40}
+                  className="rounded-md object-cover border-2 border-indigo-500"
+                />
+                <div className="flex flex-col">
+                  <h5 className="font-semibold text-sm text-indigo-800">
+                    {listing?.name.substring(0, 30) + "..."}
+                  </h5>
+                  <small className="text-xs text-gray-600">
+                    {listing?.address.substring(0, 30) + "..."}
+                  </small>
+                </div>
               </div>
-            </div>
-          ))
-        }
+            ))}
+        </div>
       </div>
     </div>
   );
--- src/app/businessSettings/devices/deviceList.tsx
+++ src/app/businessSettings/devices/deviceList.tsx
@@ -3,21 +3,26 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { DeviceTable, SeamProvider } from "@seamapi/react";
 import { useRouter } from "next/navigation";
-import { envConfig } from '@/utility/environment'
+import { envConfig } from "@/utility/environment";
 import Form from "./form";
 import CommonPopup from "@/components/commonPopup";
-import { Battery50Icon, Battery100Icon, PlusIcon, LinkIcon } from "@heroicons/react/20/solid";
+import {
+  Battery50Icon,
+  Battery100Icon,
+  PlusIcon,
+  LinkIcon,
+} from "@heroicons/react/20/solid";
 
 const DeviceList = ({ children }: any) => {
-
   const router = useRouter();
   const [clientSessionToken, setClientSessionToken] = useState("");
-  const [sifelyLocks, setSifelyLocks] = useState([])
-  const [showModal, setShowModal] = useState(false)
-  const [isLoading, setIsLoading] = useState(false)
+  const [sifelyLocks, setSifelyLocks] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [locksPressed, setLockedPress] = useState(false);
 
   const getToken = async () => {
-    const apiUrl = `${envConfig.backendUrl}/device/getclientsessiontoken`
+    const apiUrl = `${envConfig.backendUrl}/device/getclientsessiontoken`;
     const res = await axios.get(apiUrl);
     if (res.status == 200) {
       setClientSessionToken(res.data.token);
@@ -25,26 +30,68 @@ const DeviceList = ({ children }: any) => {
   };
 
   const getSifelyLocks = async () => {
-    const access_token = localStorage.getItem('sifely_access_token')
-    if (!access_token || access_token == '' || sifelyLocks.length > 0) {
-      setSifelyLocks([])
-      return
+    const access_token = localStorage.getItem("sifely_access_token");
+
+    if (!access_token || access_token == "" || sifelyLocks?.length > 0) {
+      setSifelyLocks([]);
+      return;
     }
-    setIsLoading(true)
-    const apiUrl = `${envConfig.backendUrl}/device/sifely/locklist`
-    const result = await axios.post(apiUrl, { access_token, pageNo: 1, pageSize: 100 })
-    setIsLoading(false)
-    setSifelyLocks(result.data.list)
-  }
+
+    setIsLoading(true);
+
+    const apiUrl = `${envConfig.backendUrl}/device/sifely/locklist`;
+    const result = await axios.post(apiUrl, {
+      access_token,
+      pageNo: 1,
+      pageSize: 100,
+    });
+
+    setIsLoading(false);
+    setSifelyLocks(result.data.list);
+  };
 
   const closeModal = () => {
-    setShowModal(false)
-  }
+    setShowModal(false);
+  };
 
   useEffect(() => {
     getToken();
-    getSifelyLocks()
+    getSifelyLocks();
   }, []);
+
+  useEffect(() => {
+    // This effect will run when the component mounts
+    // Check if there is a saved state in local storage
+    const savedLocksPressed = localStorage.getItem("locksPressed");
+
+    if (savedLocksPressed) {
+      setLockedPress(savedLocksPressed === "true");
+    }
+
+    const handleBeforeUnload = () => {
+      // Remove local storage data on page close
+      localStorage.removeItem("locksPressed");
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      // Cleanup the event listener
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, []);
+
+  const handleDeviceClick = (device_id: any) => {
+    setLockedPress(true);
+    localStorage.setItem("locksPressed", "true");
+    router.push(`/businessSettings/devices/seam/${device_id}`);
+  };
+
+  const handleClick = (lockId: any) => {
+    setLockedPress((prevLockedPress) => !prevLockedPress);
+    localStorage.setItem("locksPressed", "true");
+    router.push(`/businessSettings/devices/sifely/${lockId}`);
+  };
 
   return (
     <div className="h-[100%] w-[100%] bg-gray-300">
@@ -53,111 +100,141 @@ const DeviceList = ({ children }: any) => {
         onClose={() => setShowModal(false)}
         children={<Form closeModal={closeModal} />}
         disableCloseIcon={false}
-        heightwidth='100rem'
+        heightwidth="100rem"
       />
+
       <div className="flex flex-row gap-[0.05rem] h-[100%] ">
         {/* column 1 */}
-        <div className="flex-1 bg-gray-100 py-4 rounded-md w-[50%]">
-          <div className="flex justify-between px-4">
-            <h2 className="text-xl font-bold text-indigo-700">
+        <div
+          className={`flex-1 bg-white py-2 px-4 rounded-md ${
+            locksPressed ? "w-1/2" : "w-full"
+          }`}
+        >
+          <div className="flex justify-between px-4 mt-1">
+            <h2 className="text-xl font-bold text-indigo-700 mb-4">
               Smart Home Devices
             </h2>
-            <div className="flex gap-2">
+            <div className="flex  gap-2">
+              {/* Updated button styles */}
               <button
                 type="button"
-                onClick={() => router.push('/businessSettings/addDevices')}
-                className="rounded-full flex items-center bg-indigo-600 px-2 py-1 text-xs  text-white shadow-sm hover:bg-indigo-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-700"
+                onClick={() => router.push("/businessSettings/addDevices")}
+                className="rounded-md flex items-center h-7 bg-indigo-500 px-2 py-1 text-xs text-white shadow-md hover:bg-blue-600 focus:outline-none"
               >
-                <PlusIcon
-                  className="text-white mr-[4px] flex-shrink-0 h-4 w-4"
-                  aria-hidden="true"
-                />
+                <PlusIcon className="text-white  h-4 w-4" />
                 Add Devices
               </button>
+
               <button
                 type="button"
-                className="rounded-full flex items-center bg-indigo-600 px-2 py-1 text-xs  text-white shadow-sm hover:bg-indigo-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-700"
+                className="rounded-lg flex items-center h-7 bg-indigo-500 px-2 py-1 text-xs text-white shadow-md hover:bg-blue-600 focus:outline-none"
               >
-                <LinkIcon
-                  className="text-white mr-[4px] flex-shrink-0 h-4 w-4"
-                  aria-hidden="true"
-                />
+                <LinkIcon className="text-white mr-2 h-4 w-4" />
                 Pair Devices
               </button>
             </div>
           </div>
-          <div className="mt-7 h-[90%] overflow-y-auto">
+
+          <div
+            className={` ${
+              locksPressed ? "" : "flex justify-start"
+            } gap-10 mt-1 h-[90%] overflow-y-auto`}
+          >
+            {" "}
             {/* Device List here */}
             <SeamProvider clientSessionToken={clientSessionToken}>
               <DeviceTable
                 preventDefaultOnDeviceClick
-                onDeviceClick={(device_id) =>
-                  router.push(`/businessSettings/devices/seam/${device_id}`)
-                }
-                className="w-[540px] h-auto"
+                onDeviceClick={handleDeviceClick}
+                className="w-[564px] h-auto"
                 disableSearch
               />
             </SeamProvider>
-            <hr />
-            <div className="p-4">
-              <h4 className="text-slate-800 font-semibold text-lg">Sifely Locks</h4>
-              {
-                sifelyLocks.length == 0 &&
+            <div className="p-4 w-full bg-gray-50 rounded-md">
+              <h4 className="text-black text-xl font-bold mb-4">
+                Sifely Locks
+              </h4>
+
+              {sifelyLocks?.length === 0 ? (
                 <div className="text-center p-4">
-                  {
-                    !isLoading ?
-                      (
-                        <>
-                          <span className="text-slate-400 mr-4">Sign in to your sifely account</span>
-                          <span onClick={() => setShowModal(true)} className="text-blue-500 underline cursor-pointer">click here</span>
-                        </>
-                      ) : (
-                        <p>Loading...</p>
-                      )
-
-                  }
+                  {!isLoading ? (
+                    <>
+                      <span className="text-gray-500 mr-4">
+                        Sign in to your Sifely account
+                      </span>
+                      <span
+                        onClick={() => setShowModal(true)}
+                        className="text-indigo-700 underline cursor-pointer"
+                      >
+                        click here
+                      </span>
+                    </>
+                  ) : (
+                    <p>Loading...</p>
+                  )}
                 </div>
-              }
-
-              <ul className='text-slate-700 text-base font-medium mt-4'>
-                <>
-                  {
-                    sifelyLocks.map((lock: any) => (
-                      <li onClick={() => { router.push(`/businessSettings/devices/sifely/${lock.lockId}`) }} className=' p-2 rounded-md font-normal text-sm text-justify hover:bg-slate-100 flex gap-4 justify-start cursor-pointer'>
-                        <div>
-                          <img
-                            src="https://www.schlage.com/content/dam/sch-us/homepage-refresh/smart-lock.png"
-                            alt=''
-                            width={27}
-                            height={27}
-                          />
+              ) : (
+                <ul className="text-slate-700 text-base font-medium mt-4">
+                  <>
+                    {sifelyLocks?.map((lock: any) => (
+                      <li
+                        onClick={() => handleClick(lock.lockId)}
+                        className="p-2 rounded-md font-normal text-sm text-justify hover:bg-slate-100 flex items-center gap-4 justify-between border-b-2 shadow-md mb-2 cursor-pointer transition-all duration-300"
+                      >
+                        <div className="flex justify-start gap-5 mx-5">
+                          <div>
+                            <img
+                              src="https://www.schlage.com/content/dam/sch-us/homepage-refresh/smart-lock.png"
+                              alt=""
+                              width={27}
+                              height={27}
+                              className="rounded-md"
+                            />
+                          </div>
+                          <div className="flex flex-col">
+                            <h1 className="font-medium text-base text-black">
+                              {lock.lockName}
+                            </h1>
+                            <p className="text-xs text-slate-500">
+                              {lock.lockAlias}
+                            </p>
+                          </div>
                         </div>
-                        <div className='flex items-baseline'>
-                          <div className="flex flex-col ">
-                            <h1 className='font-medium text-base text-black'>{lock.lockName}</h1>
-                            <p className='text-xs text-slate-500'>{lock.lockAlias}</p>
-                          </div>
-                          <div className='flex flex-col ml-20 items-baseline text-slate-500'>
-                            <span className="text-xs ">Mac: {lock.lockMac}</span>
-                            <span className="text-xs flex gap-2">
-                              {lock.electricQuantity > 80 ? <Battery100Icon color="green" width={20} height={20} /> : <Battery50Icon color="green" width={20} height={20} />}
-                              <span>{lock.electricQuantity < 20 ? 'Low' : 'Good'} ({lock.electricQuantity}%)</span>
-                            </span>
-                          </div>
 
+                        <div className="flex flex-col ml-4 items-baseline text-slate-500">
+                          <span className="text-xs">Mac: {lock.lockMac}</span>
+                          <span className="text-xs flex items-center gap-2">
+                            {lock.electricQuantity > 80 ? (
+                              <Battery100Icon
+                                color="green"
+                                width={20}
+                                height={20}
+                              />
+                            ) : (
+                              <Battery50Icon
+                                color="green"
+                                width={20}
+                                height={20}
+                              />
+                            )}
+                            <span>
+                              {lock.electricQuantity < 20 ? "Low" : "Good"} (
+                              {lock.electricQuantity}%)
+                            </span>
+                          </span>
                         </div>
                       </li>
-                    ))
-                  }
-
-                </>
-              </ul>
+                    ))}
+                  </>
+                </ul>
+              )}
             </div>
           </div>
         </div>
-
         {/* column 2 - Device Details */}
-        <div className=" overflow-y-auto w-[50%]">{children}</div>
+        {locksPressed && (
+          <div className=" overflow-y-auto w-[50%] bg-white">{children}</div>
+        )}
       </div>
     </div>
   );
