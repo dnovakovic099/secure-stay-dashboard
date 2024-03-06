840dd6a6f55bd47e5ef64da7c3db33bd358aacdd
Nabil Yahyaoui <yahyaouinabil1993@gmail.com>
2024-03-06 21:52:31 +0100

    refact phone styling

--- src/app/upsells/createupsells/phone.tsx
+++ src/app/upsells/createupsells/phone.tsx
@@ -22,54 +22,132 @@ const PhoneStructure: React.FC<PhoneStructureProps> = ({
         </div>
 
         {/* Screen content */}
-        <div className="flex-1 justify-start bg-white overflow-hidden px-2">
-          <div className="text-indigo-700 text-sm mb-2">
+        <div className="flex-1 justify-start bg-white overflow-hidden px-2 h-[465px]">
+          <div className="text-center text-indigo-700 text-sm mb-2">
             Available Add-on service
           </div>
           <div>
-            <img
+            {/* <img
               src={image || "path_to_default_image.jpg"}
               alt="Mobile Image"
               className={`w-full h-32 rounded-md${
                 image ? "" : " border-2 border-gray-300"
               }`}
-            />
+            /> */}
+            {image ? (
+              <img
+                src={image}
+                alt="Selected"
+                className="w-full h-full object-cover rounded-md"
+              />
+            ) : (
+              <div className="border border-dashed border-[#E9ECF3] p-[5px] rounded-lg">
+                <div className="flex items-center justify-center w-full h-full px-6 py-[26px] bg-[#E9ECF3] rounded-md">
+                  <div className="flex flex-col items-center gap-[10px]">
+                    <svg
+                      width="32"
+                      height="32"
+                      viewBox="0 0 32 32"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <g clipPath="url(#clip0_9473_795)">
+                        <path
+                          d="M20 10.6666H20.0133"
+                          stroke="#6F6883"
+                          strokeWidth="1.5"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        />
+                        <path
+                          d="M16.6667 28H8C6.93913 28 5.92172 27.5786 5.17157 26.8284C4.42143 26.0783 4 25.0609 4 24V8C4 6.93913 4.42143 5.92172 5.17157 5.17157C5.92172 4.42143 6.93913 4 8 4H24C25.0609 4 26.0783 4.42143 26.8284 5.17157C27.5786 5.92172 28 6.93913 28 8V16.6667"
+                          stroke="#6F6883"
+                          strokeWidth="1.5"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        />
+                        <path
+                          d="M4 21.3333L10.6667 14.6667C11.904 13.476 13.4293 13.476 14.6667 14.6667L20 20"
+                          stroke="#6F6883"
+                          strokeWidth="1.5"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        />
+                        <path
+                          d="M18.6665 18.6666L19.9998 17.3333C20.8932 16.4746 21.9332 16.2346 22.9092 16.6133"
+                          stroke="#6F6883"
+                          strokeWidth="1.5"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        />
+                        <path
+                          d="M21.3335 25.3334H29.3335"
+                          stroke="#6F6883"
+                          strokeWidth="1.5"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        />
+                        <path
+                          d="M25.3335 21.3334V29.3334"
+                          stroke="#6F6883"
+                          strokeWidth="1.5"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        />
+                      </g>
+                      <defs>
+                        <clipPath id="clip0_9473_795">
+                          <rect width="32" height="32" fill="white" />
+                        </clipPath>
+                      </defs>
+                    </svg>
+                    <span className="text-xs leading-[14px] font-medium text-[#222222]">
+                      jpeg or png upto 2024 KB
+                    </span>
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
-          <div className="item-start mt-4">
-            <h1 className="text-sm font-bold mb-2 text-black">
-              {textContent.title || "Title"}
-            </h1>
-            <p
-              className="text-xs text-black mb-3 leading-snug"
-              style={{
-                overflowWrap: "break-word",
-                wordWrap: "break-word",
-                maxWidth: "100%",
-              }}
-            >
-              {textContent.description || "Description goes here"}
-            </p>
-            <p className="text-sm text-black mb-3">
-              {textContent.price ? (
-                <>
-                  <span className="text-green-400 font-semibold">
-                    {textContent.price}
-                  </span>{" "}
-                  Per Booking One Time
-                </>
-              ) : (
-                <>
-                  <span className="text-green-400 font-semibold">1</span> Per
-                  Booking One Time
-                </>
-              )}
-            </p>
-            <button
-              // onClick={handleSubmit}
-              className="bg-indigo-700 text-white px-2 py-2 mt-5 w-full rounded-md hover:bg-blue-600 transition-all duration-300 border-2 border-blue-700"
-            >
-              Purchase
-            </button>
+          <div className="flex flex-col justify-between item-start mt-4 h-[290px]">
+            <div>
+              <h1 className="text-sm font-bold mb-2 text-black">
+                {textContent.title || "Title"}
+              </h1>
+              <p
+                className="text-xs text-black mb-3 leading-snug"
+                style={{
+                  overflowWrap: "break-word",
+                  wordWrap: "break-word",
+                  maxWidth: "100%",
+                }}
+              >
+                {textContent.description || "Description goes here"}
+              </p>
+              <p className="text-sm text-black mb-3">
+                {textContent.price ? (
+                  <>
+                    <span className="text-green-400 font-semibold">
+                      {textContent.price}
+                    </span>{" "}
+                    Per Booking One Time
+                  </>
+                ) : (
+                  <>
+                    <span className="text-green-400 font-semibold">1</span> Per
+                    Booking One Time
+                  </>
+                )}
+              </p>
+            </div>
+            <div>
+              <button
+                // onClick={handleSubmit}
+                className="text-sm font-medium bg-gradient-to-r from-[#9E49F2] to-[#7000FF] text-[#FFFFFF] px-5 py-[10px] w-full rounded-lg"
+              >
+                Purchase
+              </button>
+            </div>
           </div>
         </div>
         <div className="absolute h-4 flex justify-center items-center right-[38%] bottom-0">
