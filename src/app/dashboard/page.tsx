4da91c8cf80a79a0af5e4ce07de924cbaf1a941e
tribikram <sentribikram@gmail.com>
2024-03-15 19:53:04 +0545

    fix: authentication in case of not logged in for every page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,34 +1,11 @@
 "use client"; //This is client component
-import { checkUserSession } from "@/auth/auth";
+
 //Component
 import SideBarMain from "@/components/sidebar";
 import { useEffect } from "react";
 
 const MainDashboard = () => {
 
-    // const userInfo = async () => {
-    //     return await checkUserSession();
-    // };
-
-    // userInfo().then((isLoggedIn) => {
-    //     if (!isLoggedIn) {
-    //         localStorage.clear();
-    //         window.location.href = '/login';
-    //     }
-    // });
-
-    useEffect(() => {
-        const userInfo = async () => {
-
-            const isLoggedIn = await checkUserSession();
-            if (!isLoggedIn) {
-                localStorage.clear();
-                window.location.href = '/login';
-            }
-        };
-
-        userInfo();
-    }, []);
 
   return (
     <SideBarMain>
